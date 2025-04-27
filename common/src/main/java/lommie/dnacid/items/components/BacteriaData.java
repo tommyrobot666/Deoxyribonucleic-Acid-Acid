feeb97f60e0d2e61824d3fdfc25701120e8fd6b9
tommyrobot666 <tommyrobot10@gmail.com>
2025-04-27 15:49:56 -0700

    Fixed BacteriaData CODEC

--- common/src/main/java/lommie/dnacid/items/components/BacteriaData.java
+++ common/src/main/java/lommie/dnacid/items/components/BacteriaData.java
@@ -4,12 +4,17 @@ import com.mojang.datafixers.util.Pair;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.DataResult;
 import com.mojang.serialization.DynamicOps;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.StreamCodec;
 import org.jetbrains.annotations.Nullable;
 
 public record BacteriaData(boolean petriDish) {
-    public static final @Nullable Codec<BacteriaData> CODEC = Codec.of(BacteriaData::encode,BacteriaData::decode);
+    public static final @Nullable Codec<BacteriaData> CODEC = RecordCodecBuilder.create(
+            (i) -> i.group(
+                    Codec.BOOL.fieldOf("petriDish").forGetter(BacteriaData::petriDish)
+            ).apply(i, BacteriaData::new)
+    );
     public static final StreamCodec<RegistryFriendlyByteBuf,BacteriaData> STREAM_CODEC = StreamCodec.of(BacteriaData::encodeStream,BacteriaData::decodeStream);
 
     private static <T> DataResult<Pair<BacteriaData,T>> decode(DynamicOps<T> tDynamicOps, T t) {
